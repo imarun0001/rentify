c77e6308c65504372428c2cb1e1747078221ded4
imarun0001 <arunanshu.patra68@gmail.com>
2024-05-28 02:12:07 +0530

    first commit

--- limehome/settings.py
+++ limehome/settings.py
@@ -80,21 +80,25 @@ WSGI_APPLICATION = 'limehome.wsgi.application'
 # Database
 #https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'limehome',
-#         'USER': 'postgres',
-#         'PASSWORD': '1234',
-#         'HOST': 'localhost',
-#     }
-# }
 DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
- }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'URL': 'postgresql://postgres:iUhhlhwwYgVaKFRqWWqVCaPOqEWizZGj@postgres.railway.internal:5432/railway',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'iUhhlhwwYgVaKFRqWWqVCaPOqEWizZGj',
+        'HOST': 'monorail.proxy.rlwy.net',
+        'PORT': '11982',
+        'OPTIONS': {
+            'options': '-c timezone=UTC'
+    },
+}}
+# DATABASES = {
+#      'default': {
+#          'ENGINE': 'django.db.backends.sqlite3',
+#          'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+#  }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -120,8 +124,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
-
+# TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 USE_I18N = True
 
 USE_L10N = True
